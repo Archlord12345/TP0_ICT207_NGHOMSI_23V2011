eb6ba833ba9f9411f537a082e6037f8f6756d05a
NGHOMSI FEUKOUO RAVEL <ravel.nghomsi@facsciences-uy1.cm>
2025-12-24 08:43:04 +0100

    initialisation diapo 24

---
+++ EX0_diapo_24.java
@@ -0,0 +1,26 @@
+public class Point{
+    double x,y;
+    Point (double a, double b){
+        int n;
+        x=a;
+        y=b;
+    }
+    Point () {
+        x=0;
+        y=0;
+    }
+    void deplacer (double dx, double dy ){
+        x+=dx;
+        y+=dy;
+    }
+}
+public class EX0_diapo_24{
+     public static void main(String[] argv){
+        int b;
+         System.out.println("\n utitialison un point p1");
+         Point p1 = new Point();
+         p1.deplacer(4,5);
+         System.out.println("\n affichons les nouvelle coordonnées de notre point ");
+         System.out.println("\n p1 a pour coordonées x="+p1.x+" et y="+p1.y);
+     }
+}
